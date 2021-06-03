809b88b125f06f5a2d92c4e289a39dbaf50e57bb
MSI\Parnkung <pakkapat.ban@coursesquare.co>
2021-06-03 10:27:48 +0700

    atsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,9 @@ module.exports = {
   {
     resolve: `gatsby-plugin-manifest`,
     options: {
-      name: `GatsbyJS`,
-      short_name: `GatsbyJS`,
-      start_url: `../images/icon.png`,
+      name: `myportfolio`,
+      short_name: `myportfolio`,
+      start_url: `../images/icon.`,
       background_color: `#f7f0eb`,
       theme_color: `#a2466c`,
       display: `standalone`,
